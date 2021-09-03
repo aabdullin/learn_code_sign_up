70574b2f3f8f001052e4c28df1de786bbe283feb
Artur <rutrika@gmail.com>
2021-09-02 21:57:16 -0700

    wip

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const form = document.querySelector('.form-container form');
-const inputs = document.querySelectorAll('.form-container input');
+const form = document.querySelector('.right-col form');
+const inputs = document.querySelectorAll('.right-col form input');
+// const form = document.querySelector('.form-container form');
+// const inputs = document.querySelectorAll('.form-container input');
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    inputs.forEach(input => {
+    inputs.forEach((input) => {
         if (!input.value) {
-            input.parentElement.classList.add('error')
+            input.parentElement.classList.add('error');
         } else {
-            input.parentElement.classList.remove('remove')
+            input.parentElement.classList.remove('error');
             if (input.type === 'email') {
                 if (validateEmail(input.value)) {
                     input.parentElement.classList.remove('error')
