c7ed01d6fb7c1b979306e93d86c1e302ea167906
Artur <rutrika@gmail.com>
2021-09-06 14:02:53 -0700

    wip

--- main.js
+++ main.js
@@ -1,31 +1,77 @@
-const form = document.querySelector('.right-col form');
-const inputs = document.querySelectorAll('.right-col form input');
-// const form = document.querySelector('.form-container form');
-// const inputs = document.querySelectorAll('.form-container input');
+class Form {
+    constructor(selector) {
+        this.fields = [];
+        this.form = document.querySelector(selector);
+        this.addField = this.addField.bind(this);
+        this.validate = this.validate.bind(this);
+    }
 
+    addField(selector, validator) {
+        const element = document.querySelector(selector);
+        this.fields.push({
+            element,
+            validator,
+        });
+    }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    inputs.forEach((input) => {
-        if (!input.value) {
-            input.parentElement.classList.add('error');
-        } else {
-            input.parentElement.classList.remove('error');
-            if (input.type === 'email') {
-                if (validateEmail(input.value)) {
-                    input.parentElement.classList.remove('error')
-                } else {
-                    input.parentElement.classList.add('error')
-                }
+    init() {
+        this.form.addEventListener('submit', this.validate);
+    }
+
+    validate(e) {
+        e.preventDefault();
+        console.log('Validate');
+        this.fields.forEach((item) => { //{ element, validator }
+            item.validator(item.element)
+        })
+    }
+};
+
+const validator = (input) => {
+    if (!input.value) {
+        input.parentElement.classList.add('error');
+    } else {
+        input.parentElement.classList.remove('error');
+        if (input.type === 'email') {
+            if (validateEmail(input.value)) {
+                input.parentElement.classList.remove('error')
+            } else {
+                input.parentElement.classList.add('error')
             }
         }
-        console.log(input.value)
-    })
-})
-
+    }
+}
 
 function validateEmail (email) {
-	let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String.email.toLowerCase())
-
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase())
 }
+
+const form = new Form('.form-container form');
+form.addField('#first_name', validator);
+form.addField('#last_name', validator);
+form.addField('#email', validator);
+form.addField('#password', validator);
+form.init();
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     inputs.forEach((input) => {
+//         if (!input.value) {
+//             input.parentElement.classList.add('error');
+//         } else {
+//             input.parentElement.classList.remove('error');
+//             if (input.type === 'email') {
+//                 if (validateEmail(input.value)) {
+//                     input.parentElement.classList.remove('error')
+//                 } else {
+//                     input.parentElement.classList.add('error')
+//                 }
+//             }
+//         }
+//         console.log(input.value)
+//     })
+// })
+
+
+
